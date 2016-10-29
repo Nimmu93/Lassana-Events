78bd490ec0ce79b871a97ff244c35ce2a6c1651a
letmadsay <desilvamadhuka@gmail.com>
2016-10-30 01:44:00 +0530

    removed css from parent but added as assets common folder for css, changed logo png to new, changed the fontstyle

--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<!----------------------------------------------------------------------------------------------------------------------->
+<!-----------------------------------------------------PHP CODING FOR LOGIN---------------------------------------------->
+<!----------------------------------------------------------------------------------------------------------------------->
 <?php
    session_start();
     include_once 'class.user.php';
@@ -5,126 +8,82 @@
     if (isset($_REQUEST['submit'])) {
         extract($_REQUEST);
         $login = $user->check_login($emailusername, $password);
-       
         if ($login == 'admin') {
             /* Registration Success*/
-           header("location:admin/orders.php");
-        } 
-        else if($login == 'stock') {
-          header("location:stock/home.php");
-        } else if($login =='clerk'){
-            
+        header("location:admin/orders.php");
+        }else if($login == 'stock') {
+            header("location:stock/home.php");
+        }else if($login =='clerk'){
             header("location:clerk/home.php");
-        }else{
-          echo    "<script type='text/javascript' language='javascript'>
-                        alert('wrong username or password');
-   
-                    </script>";
+        }else{echo    "<script type='text/javascript' language='javascript'>
+                        alert('wrong username or password');</script>";
         }
     }
-
 ?>
-   <!-- if (isset($_REQUEST['submit'])) {
-        extract($_REQUEST);
-        $login = $user->check_login($emailusername, $password);
-        
-        if ($login) {
-            $user->get_admin($emailusername, $password);
-         /*   if($employee = "stock"){
-                 /* Registration Success*/
-          /*  
-            }
-            else if ($employee = "admin"){
-                 /* Registration Success*/
-         /*   header("location:admin/orders.php");
-            }
-            
-            else {
-                 /* Registration Success*/
-         /*   header("location:clerk/home.php");
-            }*/
-           
-        } 
-        else {
-            /*registration failed*/
-            echo    "<script type='text/javascript' language='javascript'>
-                        alert('wrong username or password');
-   
-                    </script>";
-            /*header("location:home1.php");*/
-        }
-    }
-
-?> --!>
-
-
-
-
-
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!----------------------------------------------------------------------------------------------------------------------->
+<!-----------------------------------------------------END PHP CODING FOR LOGIN------------------------------------------>
+<!----------------------------------------------------------------------------------------------------------------------->
 <head>
-      <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title> Login</title>
-
+    <!-- Basic employee styles in basic css-->
+    <link href="assets/css/basic.css" rel="stylesheet" />
     <!-- BOOTSTRAP STYLES-->
-    <link href="css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <!-- FONTAWESOME STYLES-->
-    <link href="css/font-awesome.css" rel="stylesheet" />
-    <!-- GOOGLE FONTS-->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-    <style>
-        .btn:hover {
-            color:#f2f2f2;
-        }
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
     
-    </style>
-    
-
 </head>
-<body style="background-color: #656565;color:#f2f2f2">
-    <div class="container">
+
+
+<body class="login" style="font-family: 'Questrial', sans-serif;">
+    
+    <div class="container" >
+        
         <div class="row text-center " style="padding-top:100px;">
+            
             <div class="col-md-12">
+                
                 <img src="logo.png" />
+                
             </div>
+            
         </div>
-         <div class="row ">
-               
-                <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
-                           
-                            <div class="panel-body">
-                                <form role="form" method="post">
-                                    <hr />
-                                    <h5>Enter details to log in</h5>
-                                       <br />
-                                     <div class="form-group input-group">
-                                            <span class="input-group-addon"><i class="fa fa-tag"  ></i></span>
-                                           <input type="text" class="form-control"  id="exampleInputEmail1" required name="emailusername" placeholder="User name">
+        
+         
+        <div class="row ">
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                    <div class="panel-body">
+                        <form role="form" method="post">
+                            <hr />
+                                <h4>Enter details to log in</h4>
+                                    <br />
+                                        <div class="form-group input-group">
+                                            <span class="input-group-addon"><i class="fa fa-user"  ></i></span>
+                                                <input type="text" class="form-control"  id="exampleInputEmail1" required name="emailusername" placeholder="User name">
                                         </div>
-                                                                              <div class="form-group input-group">
+                                        <div class="form-group input-group">
                                             <span class="input-group-addon"><i class="fa fa-lock"  ></i></span>
                                            <input type="password" class="form-control" name="password" required id="exampleInputPassword1" placeholder="Password">
                                         </div>
-                                    
-                                     <div class="col-lg-4 text-right" >
-                                        <button class="btn btn-action" style="background-color:#124a76;" type="submit" name="submit">Sign in</button>
-                                    </div>
+                                        <div class="col-lg-4 text-right" >
+                                            <button class="login"  type="submit" name="submit">Sign in</button>
+                                        </div>
 
-                                    <p class="change_link" style="margin-top:10px; ">
-                                        <a href="forgotpasswrd/rcvrpasswrd.php" style="color:#f2f2f2" class="to_register">Forgot Your Password?</a>
-                                    </p>
-                                    <hr />
-                                   
-                                    </form>
-                            </div>
-                           
-                        </div>
-                
-                
-        </div>
+                                            <h4  style="margin-top:10px; ">
+                                                <a class="login" href="forgotpasswrd/rcvrpasswrd.php"> Forgot your password?</a>
+                                            </h4>
+                                                <hr />
+                        </form>
+                    </div>
+                </div>
+            </div>
     </div>
 
+
 </body>
+
+
+
 </html>
