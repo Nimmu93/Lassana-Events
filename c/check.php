6f4550914fa16f743cca500e8fdd5e14e7ab3c1c
letmadsay <desilvamadhuka@gmail.com>
2016-11-02 14:07:06 +0530

    added customer folder as folder c

---
+++ c/check.php
@@ -0,0 +1,358 @@
+<!-- === BEGIN HEADER === -->
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+    <!--<![endif]-->
+    <head>
+        <!-- Title -->
+        <title>Lassana Party | Check Availability </title>
+        <!-- Meta -->
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <!-- Favicon -->
+        <link href="favicon.ico" rel="shortcut icon">
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/bootstrap.css" rel="stylesheet">
+        <!-- CSS Stylesheets-->
+        <link rel="stylesheet" href="assets/css/animate.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/font-awesome.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/lassana.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/responsive.css" rel="stylesheet">
+        <!-- Google Fonts-->
+        <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=PT+Sans" type="text/css" rel="stylesheet">
+        <link href="http://fonts.googleapis.com/css?family=Roboto:400,300" rel="stylesheet" type="text/css">
+        
+        <style>
+            th{
+                padding-top: 15px;
+                padding-bottom: 30px;
+                padding-right: 150px;
+                padding-left: 10px;
+            } 
+            
+            
+        </style>
+    </head>
+    <body>
+        <div id="body-bg">
+            
+            
+            <div id="header">
+                <div class="container">
+                    <div class="row">
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a href="index.php" title="">
+                                <img src="assets/img/logo.png" alt="Logo" height= />
+                            </a>
+                        </div>
+                        <!-- End Logo -->
+                    </div>
+                </div>
+            </div>
+            <!-- Top Menu -->
+            <div id="hornav" class="container no-padding">
+                <div class="row">
+                    <div class="col-md-12 no-padding">
+                        <div class="text-center visible-lg">
+                            <ul id="hornavmenu" class="nav navbar-nav">
+                                <li>
+                                    <a href="index.html" class="fa-home">Home</a>
+                                </li>
+                                <li>
+                                    <span class="fa-table">Furniture</span>
+                                    <ul>
+                                        <li>
+                                            <a href="chairs.php">Chairs</a>
+                                        </li>
+                                        <li>
+                                            <a href="tables.html">Tables</a>
+                                        </li>
+                                        
+                                        
+                                    </ul>
+                                </li>
+                                
+                                <li>
+                                    <a href="tents.html"><span class="fa-flag">Tents</span></a>
+                                    
+                                </li>
+                                
+                                <li>
+                                    <a href="catering.html"><span class="fa-spoon">Catering</span> </a>
+                                    
+                                </li>
+                                
+                                 <li>
+                                    <span class="fa-gears">More Service</span>
+                                    <ul>
+                                        <li>
+                                            <a href="cutlery.html">Cutlery</a>
+                                        </li>
+                                        <li>
+                                            <a href="crockery.html">Crockery</a>
+                                        </li>
+                                        <li>
+                                            <a href="glassware.html">Glassware</a>
+                                        </li>
+                                        <li>
+                                            <a href="stage.html">Stage & Band Setup</a>
+                                        </li>
+                                        <li>
+                                            <a href="dishes.html">Chaffing Dishes</a>
+                                        </li>
+                                        <li>
+                                            <a href="bowls.html">Soup and Dessert Bowls</a>
+                                        </li>
+                                        
+                                    </ul>
+                                </li>
+                                
+                                <li>
+                                    <span class="fa-camera">Gallery</span>
+                                    <ul>
+                                        <li>
+                                            <a href="weddings.html">Wedding</a>
+                                        </li>
+                                        <li>
+                                            <a href="parties.html">Party</a>
+                                        </li>
+                                        <li>
+                                            <a href="Official.html">Official</a>
+                                        </li>
+                                        
+                                    </ul>
+                                </li>
+                                
+                                <li>
+                                    <a href="contact.html"><span class="fa-phone">Contact</span></a>
+                                    
+                                </li>
+                            
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Top Menu -->
+        
+            <!-- === END HEADER === -->
+            
+            <!-- === start content === -->
+           <div id="content">
+             
+                <div class="container background-white">
+                    <div class="row margin-vert-30">
+                        <!-- Main Column -->
+                        <div class="col-md-12"><center>
+                            <!-- Main Content -->
+                            <div class="headline" >
+                                <h2>Reservation</h2>
+                                <center>
+                            <form> 
+                            <table style="margin:0 0 1% 60%; width:40%;background-color: lightgrey; border-radius: 5px;color: #124a76;">
+                                
+                                <tr> 
+                                    
+                                    <th colspan="4" style="padding-top: 5px;padding-bottom: 5px;padding-right: 10px;padding-left: 10px;text-align:center" >Item List 
+                                    </th>
+                                </tr>
+                                    
+                                <tr>
+                                    <th style="padding-top: 5px;padding-bottom: 5px;padding-right: 10px;padding-left: 10px;">Item Amount </th>
+                                    <td class="form-control" style="padding-top: 2px;padding-bottom: 0;padding-right: 40px;padding-left: 40px;"></td> <!--using php session varibales show the selected number of items-->
+                                    <th style="padding-top: 5px;padding-bottom: 5px;padding-right: 10px;padding-left: 10px;">Total Amount (Rs.) </th>
+                                    <td class="form-control" style="padding-top: 2px;padding-bottom: 0;padding-right: 40px;padding-left: 40px;"></td> 
+                                </tr>
+                                
+                                
+                            </table>
+                            </form>
+                            </center>
+                            </div>
+                            
+                           
+                            <!-- Item Form -->
+                            <table>
+
+                            <form method="post" action=""> 
+                                <tr>
+                                    <th>Item Type </th>
+                                    <td><input class="form-control" type="text" name="item_type"></td> <!--using php session varibales show the selected item type as in chair/tent/table -->
+                                </tr>
+                                
+                                <tr>
+                                    <th>Item Name </th>
+                                    <td><input class="form-control" type="text" name="item"></td> <!--using php session varibales show the selected item name as in square/circular/ -->
+                                </tr>
+                                
+                                <tr>
+                                    <th >Color <span class="color-red">*</span></th>
+                                    <td ><select class="form-control" name="colors"><option value="white">White</option> </select></td><!--derive drop down list based on the item chosen in the previous page-->
+                                </tr>
+                                
+                                <tr>
+                                    <th >Quantity <span class="color-red">*</span></th>
+                                    <td > <input class="form-control" type="text" name="quantity"></td> 
+                                </tr>
+                                <tr>
+                                    <th>Date(From) <span class="color-red">*</span></th>
+                                    <td> <input class="form-control" type="date" name="date"></td> 
+                                </tr>
+                                <tr>
+                                    <th>Time(From) <span class="color-red">*</span></th>
+                                    <td> <input class="form-control" type="time" name="date"></td> 
+                                </tr>
+                                <tr>
+                                    <th>Date(To) <span class="color-red">*</span></th>
+                                    <td> <input class="form-control" type="date" name="date"></td> 
+                                </tr>
+                                <tr>
+                                    <th>Time(To) <span class="color-red">*</span></th>
+                                    <td> <input class="form-control" type="time" name="date"></td> 
+                                </tr>
+                               
+                                <tr>
+                                    <td></td>
+                                    <td> <center><input type="reset"></center> </td>
+                                </tr>
+                                
+                                <tr>
+                                    <td><p></p></td>
+                                    <td></td>
+                                </tr>
+                                
+                                <tr>
+                                    <td><button type="submit" name="ava" id="ava" style="width:60%" >Check Availability</button>
+
+    <!-- Algorithm to check availability of an item-->
+                                    <?php
+                                        include 'db_confg.php';
+
+                                       
+
+                                        if(isset($_POST['ava'])){
+                                            $date= $_POST['date'];
+                                            $item= $_POST['item'];
+                                            $quantity= $_POST['quantity'];
+
+                                            $date2 = mysqli_query($conn,"SELECT Date FROM item");
+                                            $row =mysqli_fetch_assoc($date2);
+                                            $result2= $row['Date'];
+
+                                            $item2 = mysqli_query($conn,"SELECT Item_Name FROM item");
+                                            $row2=mysqli_fetch_assoc($item2);
+                                            $result3= $row2['Item_Name'];
+
+                                            $item3 = mysqli_query($conn,"SELECT Item_Name FROM stock");
+                                            $row3=mysqli_fetch_assoc($item3);
+                                            $result6= $row3['Item_Name'];
+
+                                            $stock = mysqli_query($conn,"SELECT Stock_In FROM item");
+                                            $row3=mysqli_fetch_assoc($stock);
+                                            $result4= $row3['Stock_In'];
+
+                                            $stock2= mysqli_query($conn,"SELECT Total_Stock FROM stock");
+                                            $row4=mysqli_fetch_assoc($stock2);
+                                            $result5= $row4['Total_Stock'];
+
+                                            
+                                                if ($result2==$date && $result3==$item){
+                                                    if($result4>=$quantity){
+                                                        echo "<script type='text/javascript' language='javascript'>
+                                                            alert('You can order' );
+    
+                                                            </script>";
+                                                    }
+                                                    else{
+                                                    echo "<script type='text/javascript' language='javascript'>
+                                                            alert('You can not order');
+    
+                                                            </script>";
+                                                    }
+                                                }else if($result5>=$quantity && $result6 == $item ){
+
+                                                    echo "<script type='text/javascript' language='javascript'>
+                                                            alert('You can order');
+    
+                                                            </script>";
+                                                }else{
+                                                    echo "<script type='text/javascript' language='javascript'>
+                                                            alert('You can not order');
+    
+                                                            </script>";
+                                                }
+                                            
+                                        }
+                                                                                 
+                                    
+                                    ?>
+<!--end of the algorithm-->
+                                    </td>
+                                    <td><center><input type="submit" class="btn " value="Check Cost" style="width:70%"></center></td>
+
+                                </tr>                        
+                            
+                            </form>
+                            </table>
+                           
+                            <!-- End Item Form -->
+                            <!-- End Main Content -->
+                               
+                        </div>
+                        <!-- End Main Column -->
+                        
+                        </div>
+                        <!-- End Side Column -->
+                    </div>
+                </div>
+           
+            <!-- === END CONTENT === -->
+            <!-- === END CONTENT === -->
+            
+            
+                
+            
+            <!-- Footer Menu -->
+            <div id="footer">
+                <div class="container">
+                    <div class="row">
+                        
+                        <div id="copyright" class="col-md-12">
+                            
+                            <p style="text-align:center;color:#124a76">www.lassanaparty.com <span class="glyphicon glyphicon-copyright-mark"></span>   2016 | Designed by Root Fixers</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Footer Menu -->
+            <!-- JS -->
+          
+         
+            <!--<script src="https://code.jquery.com/jquery-1.12.4.js" integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU=" crossorigin="anonymous"></script> -->
+            <script type="text/javascript" src="assets/js/jquery.min.js" type="text/javascript"></script>
+            <script type="text/javascript" src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+            <script type="text/javascript" src="assets/js/scripts.js"></script>
+            <!-- Isotope - Portfolio Sorting -->
+            <script type="text/javascript" src="assets/js/jquery.isotope.js" type="text/javascript"></script>
+            <!-- Mobile Menu - Slicknav -->
+            <script type="text/javascript" src="assets/js/jquery.slicknav.js" type="text/javascript"></script>
+            <!-- Animate on Scroll-->
+            <script type="text/javascript" src="assets/js/jquery.visible.js" charset="utf-8"></script>
+            <!-- Sticky Div -->
+            <script type="text/javascript" src="assets/js/jquery.sticky.js" charset="utf-8"></script>
+            <!-- Slimbox2-->
+            <script type="text/javascript" src="assets/js/slimbox2.js" charset="utf-8"></script>
+            <!-- this zooms the images-->
+            <script type="text/javascript" src="assets/js/zooming.js" charset="utf-8"></script>
+            <!-- End JS -->
+    </body>
+</html>
+<!-- === END FOOTER === -->
--- clerk/orders.php
+++ clerk/orders.php
@@ -22,13 +22,13 @@
     <title>Orders</title>
 
     <!-- BOOTSTRAP STYLES-->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="../assets/css/bootstrap.css" rel="stylesheet" />
     <!-- FONTAWESOME STYLES-->
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="../assets/css/font-awesome.css" rel="stylesheet" />
     <!--CUSTOM BASIC STYLES-->
-    <link href="assets/css/basic.css" rel="stylesheet" />
+    <link href="../assets/css/basic.css" rel="stylesheet" />
     <!--CUSTOM MAIN STYLES-->
-    <link href="assets/css/custom.css" rel="stylesheet" />
+    <link href="../assets/css/custom.css" rel="stylesheet" />
     <!-- GOOGLE FONTS-->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 </head>
