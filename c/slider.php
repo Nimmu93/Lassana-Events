d72307ee092ed43528b5778b745d0d71d1a9f5c6
letmadsay <desilvamadhuka@gmail.com>
2016-11-16 17:57:10 +0530

    changed slider images and added simplified js file

--- c/slider.php
+++ c/slider.php
@@ -4,87 +4,40 @@
     <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
     <link href="../assets/css/web/slider.css" rel="stylesheet" type="text/css" />
     <script src="../assets/js/web/slider.js" type="text/javascript"></script>
-    <style>
-        body {font: normal 0.9em Arial;margin:0;}
-        a {color:#1155CC;}
-        ul li {padding: 10px 0;}
-        header {display:block;padding:60px 0 20px;text-align:center;position:absolute;top:8%;left:8%;z-index:4;}
-        header a {
-            font-family: sans-serif;
-            font-size: 24px;
-            line-height: 24px;
-            padding: 8px 13px 7px;
-            color: #fff;
-            text-decoration:none;
-            transition: color 0.7s;
-        }
-        header a.active {
-            font-weight:bold;
-            width: 24px;
-            height: 24px;
-            padding: 4px;
-            text-align: center;
-            display:inline-block;
-            border-radius: 50%;
-            background: #C00;
-            color: #fff;
-        }
-    </style>
-</head>
-<body>
     
-    <!--start-->
-    <div id="ninja-slider">
-        <div class="slider-inner">
-            <ul>
-                <li>
-                    <a class="ns-img" href="../assets/img/website/slider/1.jpg"></a>
-                    <div class="caption" id="capblack">Online Reservation</div>
-                </li>
-                <li>
-                    <a class="ns-img" href="../assets/img/website/slider/2.jpg"></a>
-                    <div class="caption" id="capblack">Marquees</div>
-                </li>
-                <li>
-                    <a class="ns-img" href="../assets/img/website/slider/3.jpg"></a>
-                    <div class="caption" id="capblack">Chairs</div>
-                </li>
-                <li>
-                    <a class="ns-img" href="../assets/img/website/slider/4.jpg"></a>
-                    <div class="caption" id="capblack">Tables</div>
-                </li>
-                <li>
-                    <a class="ns-img" href="../assets/img/website/slider/5.jpg"></a>
-                    <!--<div class="caption" id="cappink">Event Decorations</div>-->
-                </li>
-                <li>
-                    <a class="ns-img" href="../assets/img/website/slider/6.jpg"></a>
-                    <!--<div class="caption">Events to remember</div>-->
-                </li>
-                <li>
-                    <a class="ns-img" href="../assets/img/website/slider/7.jpg"></a>
-                    <!--<div class="caption">Anywhere</div>-->
-                </li>
-                <li>
-                    <a class="ns-img" href="../assets/img/website/slider/8.jpg"></a>
-                    <!--<div class="caption">Any moment</div>-->
-                </li>
-                <li>
-                    <a class="ns-img" href="../assets/img/website/slider/9.jpg"></a>
-                    <!--<div class="caption">Anytime</div>-->
-                </li>
-                <li>
-                    <a class="ns-img" href="../assets/img/website/slider/10.jpg"></a>
-                    <div class="caption" id="capwhite">Lassana-Events</div>
-                </li>
-            </ul>
-            <div class="navsWrapper">
-                <div id="ninja-slider-prev"></div>
-                <div id="ninja-slider-next"></div>
-            </div>
-        </div>
-    </div>
-    <!--end-->
-   
+
+<style>
+.mySlides {display:none;}
+
+</style>
+<body>
+
+
+
+<div >
+  <img class="mySlides" src="../assets/img/website/slider/dummy1.jpg" style="width:100%">
+  <img class="mySlides" src="../assets/img/website/slider/dummy2.jpg" style="width:100%">
+  <img class="mySlides" src="../assets/img/website/slider/dummy3.jpg" style="width:100%">
+  <img class="mySlides" src="../assets/img/website/slider/dummy5.jpg" style="width:100%">
+  
+</div>
+
+<script>
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2500); // Change image every 2 seconds
+}
+</script>
+
 </body>
 </html>
